4748c9fa88874edd6da8b0d24b35952e712dd9e0
arpit <arpitsahu789@gmail.com>
2022-12-20 22:27:30 +0530

    added Navigation to product detail Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './screens/product_overview_screen.dart';
+import './screens/product_detail_screen.dart';
 
 void main(){
 runApp(MyApp());
@@ -11,7 +12,16 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       title: 'My Shop App',
+      theme: ThemeData(
+        //appBarTheme: AppBarTheme(color: Colors.purple),
+        primaryColor: Colors.deepOrange,
+        primarySwatch: Colors.purple,
+        fontFamily: 'Lato',
+      ),
       home: ProductOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName : (ctx)=>ProductDetailScreen(),
+      },
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -13,13 +14,20 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(imageUrl,fit: BoxFit.cover,),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        title: Text(title,textAlign: TextAlign.center,),
-        leading:  IconButton(icon: Icon(Icons.favorite),onPressed: (){}, )  ,
-        trailing: IconButton(icon:  Icon(Icons.shopping_cart),onPressed: (){}, ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          title: Text(title,textAlign: TextAlign.center,),
+          leading:  IconButton(icon: Icon(Icons.favorite),onPressed: (){},color: Theme.of(context).primaryColor, )  ,
+          trailing: IconButton(icon:  Icon(Icons.shopping_cart),onPressed: (){},color: Theme.of(context).primaryColor, ),
+        ),
+
+        child: GestureDetector(child: Image.network(imageUrl,fit: BoxFit.cover,),onTap: (){
+          Navigator.of(context).pushNamed(ProductDetailScreen.routeName,arguments: id);
+
+        },),
       ),
     );
   }
