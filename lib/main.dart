a24496ddef4b020b478e7d1fd9c74fb953be8f4e
arpit <arpitsahu789@gmail.com>
2022-12-20 15:02:16 +0530

    made product overview screen

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import './screens/product_overview_screen.dart';
+
+void main(){
+runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'My Shop App',
+      home: ProductOverviewScreen(),
+    );
+  }
+}
---
+++ lib/modals/product.dart
@@ -0,0 +1,18 @@
+class Product{
+
+  final String id;
+  final String title;
+  final double price;
+  final String description;
+  final String imageUrl;
+  bool isFavorite;
+
+  Product({
+  required  this.id,
+  required  this.title,
+  required  this.price,
+  required  this.description,
+  required  this.imageUrl,
+    this.isFavorite = false,
+});
+}
