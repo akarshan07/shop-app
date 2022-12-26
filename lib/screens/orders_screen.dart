e3b201268c401367d7d786b2a3be6d683fd1a2b8
arpit <arpitsahu789@gmail.com>
2022-12-26 16:34:40 +0530

    completed module 3

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,19 +4,35 @@ import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 
-class OrderScreen extends StatelessWidget {
+class OrderScreen extends StatefulWidget {
 
   static const routeName = '/order-screen';
-  
+
+  @override
+  State<OrderScreen> createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+
+  Future? _ordersFuture;
+
+  Future obtainOrderFuture(){
+    return Provider.of<Orders>(context,listen: false).fectchAndSetOrders();
+  }
+
+  @override
+  void initState() {
+    _ordersFuture = obtainOrderFuture();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-
 
     return Scaffold(
       appBar: AppBar(title: const Text('Yours Order'),),
       drawer: AppDrawer(),
       body: FutureBuilder(
-        future: Provider.of<Orders>(context,listen: false).fectchAndSetOrders(),
+        future: _ordersFuture,
         builder: (ctx,dataSnapshot){
           if(dataSnapshot.connectionState == ConnectionState.waiting){
             return Center(child: CircularProgressIndicator(),);
