1b5dfe33e8490179743e2bd8608b0d60c495e507
arpit <arpitsahu789@gmail.com>
2023-01-04 23:21:17 +0530

    modified code

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,9 +8,9 @@ class Auth with ChangeNotifier{
   String? _userId;
   DateTime? _dateTime;
 
-  Future<void> signUp(String email,String password) async{
 
-    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAAgvlf7aQ6LwaFLZrVUZszAPzizAeU_Ws');
+  Future<void> _authenticate(String email, String password, String urlSegment) async{
+    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyAAgvlf7aQ6LwaFLZrVUZszAPzizAeU_Ws');
     final response = await http.post(url,body: json.encode({
       'email':email,
       'password':password,
@@ -20,16 +20,16 @@ class Auth with ChangeNotifier{
     print(json.decode(response.body));
   }
 
+  Future<void> signUp(String email,String password) async{
+
+   return _authenticate(email, password, 'signUp');
+
+  }
+
   Future<void> signIn( String email,String password ) async{
 
-    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAAgvlf7aQ6LwaFLZrVUZszAPzizAeU_Ws');
-    final response = await http.post(url,body: json.encode({
-      'email':email,
-      'password':password,
-      'returnSecureToken':true,
-    }));
-
-    print(json.decode(response.body));
+    return _authenticate(email, password, 'signInWithPassword');
+    
   }
 
 }
