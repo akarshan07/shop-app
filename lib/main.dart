e678ceef771d226e0b79aec8cde1ad7f6a8f0bb4
arpit <arpitsahu789@gmail.com>
2023-01-04 22:52:34 +0530

    added signup method

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter/services.dart';
 import './providers/cart.dart';
 import 'package:provider/provider.dart';
 import './screens/product_overview_screen.dart';
+import './providers/auth.dart';
 import './screens/orders_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/user_product_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 import './providers/products.dart';
 import './providers/orders.dart';
 
@@ -24,6 +26,8 @@ class MyApp extends StatelessWidget{
     ]
     );
     return MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (ctx)=>Auth(),),
         ChangeNotifierProvider(
         create: (ctx)=> Products(),),
         ChangeNotifierProvider(
@@ -40,7 +44,7 @@ class MyApp extends StatelessWidget{
           chipTheme: ChipThemeData(backgroundColor: Colors.purple.shade500,labelStyle: const TextStyle(color: Colors.white)),
           fontFamily: 'Lato',
         ),
-        home: ProductOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName : (ctx)=>ProductDetailScreen(),
           CartScreen.routeName: (ctx)=> CartScreen(),
---
+++ lib/providers/auth.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Auth with ChangeNotifier{
+
+  String? _token;
+  String? _userId;
+  DateTime? _dateTime;
+
+  Future<void> signUp(String email,String password) async{
+
+    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAAgvlf7aQ6LwaFLZrVUZszAPzizAeU_Ws');
+    final response = await http.post(url,body: json.encode({
+      'email':email,
+      'password':password,
+      'returnSecureToken':true,
+    }));
+
+    print(json.decode(response.body));
+  }
+
+
+}
